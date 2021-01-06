28367b333c3377acba2f6af9090c3d412ba96da7
Jack Boespflug <jack.boespflug@kineticdata.com>
2021-01-06 13:54:38 -0600

    Formatting and algorithm settings.

--- lib/helpers.sh
+++ lib/helpers.sh
@@ -2,14 +2,14 @@
 set -eu
 [ "${BASH_VERSINFO[0]}" -ge 3 ] && set -o pipefail
 
-get_platform () {
+get_platform() {
   local silent=${1:-}
   local platform=""
 
   platform="$(uname | tr '[:upper:]' '[:lower:]')"
 
   case "$platform" in
-    linux|darwin)
+    linux | darwin)
       [ -z "$silent" ] && msg "Platform '${platform}' supported!"
       ;;
     *)
@@ -20,15 +20,15 @@ get_platform () {
   echo -n "$platform"
 }
 
-msg () {
+msg() {
   echo -e "\033[32m$1\033[39m" >&2
 }
 
-err () {
+err() {
   echo -e "\033[31m$1\033[39m" >&2
 }
 
-fail () {
+fail() {
   err "$1"
   exit 1
 }

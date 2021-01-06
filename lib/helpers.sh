9f8ceed7e3e64568b999454ffa95716ff6c07e65
Jack Boespflug <jack.boespflug@kineticdata.com>
2021-01-06 13:42:15 -0600

    Formatting

--- lib/helpers.sh
+++ lib/helpers.sh
@@ -3,32 +3,32 @@ set -eu
 [ "${BASH_VERSINFO[0]}" -ge 3 ] && set -o pipefail
 
 get_platform () {
-    local silent=${1:-}
-    local platform=""
+  local silent=${1:-}
+  local platform=""
 
-    platform="$(uname | tr '[:upper:]' '[:lower:]')"
+  platform="$(uname | tr '[:upper:]' '[:lower:]')"
 
-    case "$platform" in
-        linux|darwin)
-            [ -z "$silent" ] && msg "Platform '${platform}' supported!"
-            ;;
-        *)
-            fail "Platform '${platform}' not supported!"
-            ;;
-    esac
+  case "$platform" in
+    linux|darwin)
+      [ -z "$silent" ] && msg "Platform '${platform}' supported!"
+      ;;
+    *)
+      fail "Platform '${platform}' not supported!"
+      ;;
+  esac
 
-    echo -n "$platform"
+  echo -n "$platform"
 }
 
 msg () {
-    echo -e "\033[32m$1\033[39m" >&2
+  echo -e "\033[32m$1\033[39m" >&2
 }
 
 err () {
-    echo -e "\033[31m$1\033[39m" >&2
+  echo -e "\033[31m$1\033[39m" >&2
 }
 
 fail () {
-    err "$1"
-    exit 1
+  err "$1"
+  exit 1
 }
